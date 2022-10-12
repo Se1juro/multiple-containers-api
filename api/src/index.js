e992687084f6795f404997a91ae96207c37d132f
Daniel Morales <mralejo99@gmail.com>
2022-10-12 00:15:49 -0500

    Added web service to docker

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const productRoutes = require("./routes/products.routes");
 // Constants
 const PORT = process.env.PORT;
 
 // App
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(productRoutes);
